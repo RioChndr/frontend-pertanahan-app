4dfdd51b3a2cb7d29fd0698732bf2cfb9640c6c3
Rio Chandra <riochandra4@gmail.com>
2021-07-03 22:57:15 +0700

    fix: navigation algorithm

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -6,36 +6,28 @@ if (localStorage.getItem(process.env.VUE_APP_TOKEN_STORAGE) !== null) {
   userData = JSON.parse(localStorage.getItem(process.env.VUE_APP_USER_INFO));
   userRole = userData.role;
 
-  if (userRole.id === 4) {
-    menus = [
-      { path: "/submissions", title: "Data Permohonan" },
-      { path: "/archive", title: "Cetak SPS" },
-      { path: "/history", title: "Riwayat Permohonan" }
-    ];
-  } else if ([2, 6].includes(userRole.id)) {
-    menus = [{ path: "/delivery", title: "Kirim & Jemput Berkas" }];
-  } else if (userRole.id === 1) {
-    menus = [
-      { path: "/dashboard", title: "Dashboard" },
-      { path: "/archive-logs", title: "Arsip Logs" }
-    ];
-  } else if (userRole.id === 5) {
-    menus = [
-      { path: "/dashboard", title: "Dashboard" },
-      { path: "/history", title: "Riwayat Permohonan" },
-      { path: "/delivery", title: "Kirim & Jemput Berkas" },
-      { path: "/request", title: "Permohonan" },
-      { path: "/setting/services", title: "Jenis Pelayanan" },
-      { path: "/setting/users", title: "User Management" }
-      // { path: '/setting/roles', title: "Role User" },
-    ];
-  } else if (userRole.id === 3) {
-    menus = [
-      { path: "/request/list", title: "Permohonan" },
-      { path: "/request/create", title: "Buat Permohonan" },
-      { path: "/delivery", title: "Kirim & Jemput Berkas" }
-    ];
-  }
+  // userRole.id has number in [1,2,3,4,5,6]
+  let configMenus = [
+    { path: "/dashboard", title: "Dashboard", rules: [1,5] },
+    
+    { path: "/submissions", title: "Data Permohonan", rules: [4] },
+    { path: "/archive", title: "Cetak SPS", rules: [4] },
+    { path: "/history", title: "Riwayat Permohonan", rules: [4, 5] },
+    
+    { path: "/delivery", title: "Kirim & Jemput Berkas", rules: [2,6,5,3] },
+    { path: "/archive-logs", title: "Arsip Logs", rules: [1] },
+    
+    { path: "/request", title: "Permohonan", rules: [5] },
+    { path: "/setting/services", title: "Jenis Pelayanan", rules: [5] },
+    { path: "/setting/users", title: "User Management", rules: [5] },
+
+    { path: "/request/list", title: "Permohonan", rules:[3] },
+    { path: "/request/create", title: "Buat Permohonan", rules:[3] },
+
+    { path: "/account/setting", title: "Pengaturan Akun", rules: [1,2,3,4,5,6]}
+  ]
+  
+  menus = configMenus.filter(v => v.rules.includes(userRole.id))
 }
 
 export default menus;
