584328d60cde46301488bf132780a1141848d446
Rio Chandra <riochandra4@gmail.com>
2021-07-04 23:24:33 +0700

    fix: change status component into outlined

--- src/components/StatusComponent/Status.vue
+++ src/components/StatusComponent/Status.vue
@@ -1,6 +1,6 @@
 <template>
   <span>
-    <span class="badge" :class="'badge-' + getColor">
+    <span class="badge-outlined" :class="'badge-outlined-' + getColor">
       {{ getText }}
     </span>
   </span>
@@ -55,6 +55,35 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.badge-outlined{
+  padding: .5em .4em;
+  border:1px solid;
+  border-radius:8px;
+  background:white;
+  &.badge-outlined-primary{
+    border-color: #007bff;
+    color: #007bff;
+  }
+  &.badge-outlined-secondary{
+    border-color: #6c757d;
+    color: #6c757d;
+  }
+  &.badge-outlined-success{
+    border-color: #28a745;
+    color: #28a745;
+  }
+  &.badge-outlined-danger{
+    border-color: #dc3545;
+    color: #dc3545;
+  }
+  &.badge-outlined-warning{
+    border-color: #ffc107;
+    color: #ffc107;
+  }
+  &.badge-outlined-dark{
+    border-color: #343a40;
+    color: #343a40;
+  }
+}
 </style>
