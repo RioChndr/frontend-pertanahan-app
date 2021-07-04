9c17120d2c987a5c6819ae0c1b62c76ede5a4d0f
Rio Chandra <riochandra4@gmail.com>
2021-07-04 21:12:16 +0700

    feat: add component status permohonan

---
+++ src/components/StatusPermohonan/Status.vue
@@ -0,0 +1,60 @@
+<template>
+  <span>
+    <span class="badge" :class="'badge-' + getColor">
+      {{ getText }}
+    </span>
+  </span>
+</template>
+
+<script>
+import configStatus from "./configStatus";
+
+export default {
+  props:{
+    permohonan: {
+      type: Boolean,
+      default: false,
+    },
+    arsipLogs: {
+      type: Boolean,
+      default: false,
+    },
+    delivery: {
+      type: Boolean,
+      default: false,
+    },
+    status: null,
+  },
+  data() {
+    return {
+      configStatus,
+    }
+  },
+  computed:{
+    typeStatus(){
+      if(this.permohonan) return "permohonan"
+      if(this.arsipLogs) return "arsipLogs"
+      if(this.delivery) return "delivery"
+      else return "permohonan" // default
+    },
+    getColor(){
+      if(this.configStatus[this.typeStatus][this.status]){
+        return this.configStatus[this.typeStatus][this.status].color
+      } else {
+        return "secondary" // default
+      }
+    },
+    getText(){
+      if(this.configStatus[this.typeStatus][this.status]){
+        return this.configStatus[this.typeStatus][this.status].text
+      } else{
+        return "secondary" // default
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/plugins/globalComponents.js
+++ src/plugins/globalComponents.js
@@ -9,6 +9,7 @@ import vSelect from "vue-select";
 import LoadingOverlay from "vue-loading-overlay";
 import "vue-loading-overlay/dist/vue-loading.css";
 import { getStatusArchive, getStatusSubmission } from "../helpers/utils";
+import StatusPermohonan from "@/components/StatusPermohonan/Status";
 
 // import Filter
 
@@ -20,6 +21,7 @@ const GlobalComponents = {
     Vue.component("p-button", Button);
     Vue.component("v-select", vSelect);
     Vue.component("v-loading", LoadingOverlay);
+    Vue.component("status-permohonan", StatusPermohonan);
     Vue.filter("getStatusValue", value => {
       if (value) {
         return getStatusSubmission(value);
